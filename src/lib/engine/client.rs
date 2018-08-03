9c2746f7db1ca4812f1d04ad0f3f779aa53620fc
Ilya Druzhinin <ilya.druzh@gmail.com>
2018-08-03 19:08:08 +0300

    new client methods

--- src/lib/engine/client.rs
+++ src/lib/engine/client.rs
@@ -1,6 +1,30 @@
-fn u32_to_data_chunk() {}
+pub type data_chunk = Vec<u8>;
+pub type ec_compressed = Vec<usize>;
 
-fn xor() {}
+const EC_COMPRESSED_SIZE: usize = 33;
+
+fn u32_to_data_chunk(value: u32) -> data_chunk {
+    let mut return_value: data_chunk = Vec::new();
+    for x in (0..3).rev() {
+        return_value.push((value >> (8 * x)) as u8);
+    }
+    println!("return_value in u32_to_data_chunk: {:?}\n", &return_value);
+
+    return return_value;
+}
+
+// побитовое ИСКЛЮЧАЮЩЕЕ ИЛИ
+fn xor(point1: ec_compressed, point2: ec_compressed) -> ec_compressed {
+    let mut return_value: ec_compressed = Vec::new();
+
+    for x in (0..EC_COMPRESSED_SIZE) {
+        return_value[x] = point1[x] ^ point2[x];
+    }
+
+    println!("return_value in XOR: {:?}\n", &return_value);
+
+    return return_value;
+}
 
 fn send_transaction() {}
 
